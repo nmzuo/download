2a1119da64c7b0185397733705e0db10abb90073
nmzuo <nmzuo@nlpr.ia.ac.cn>
2018-04-10 10:28:45 +0800

    Convert ROI file (NIFTI) to CAT12 ROI format

---
+++ convert_BNAtlas_cat12/convert_BNAtlas_cat12.m
@@ -0,0 +1,50 @@
+function convert_BNAtlas_cat12
+
+% format for the spm/VBM cat12 template
+% #1: ROIid;ROIabbr;ROIname;ROIbaseid;ROIbasename;Voxel;Volume;XYZ
+% by NM Zuo, Feb. 20, 2018
+% nmzuo@nlpr.ia.ac.cn
+% Institute of Automation, Chinese Academy of Sciences
+
+
+BN=load_nii('BN_Atlas_246_1.5mm_SPM.nii');
+nROI=length(unique(BN.img(:))) -1; % remove 0
+BN=BN.img;
+
+mycoord=load('BN_Atlas_246_coord.txt');
+myname=readtable('subregion_name.txt', 'delimiter', ' ');
+
+if 0 % generate CSV file
+    outfile='BN_Atlas_246_1.5mm_SPM.csv';
+    fid=fopen(outfile, 'w');
+    VOL=1.5^3/1000;
+    for i=1:nROI
+        Vox = length(find(BN==i));
+        Vol = Vox*VOL;
+        fprintf(fid, '%d;%s;%s;[ %d ];%s;%d;%0.2f;%d,%d,%d\n',   ...
+            i, myname.Var3{i}, myname.Var3{i},  ...
+            i, myname.Var3{i}, Vox, Vol,  ...
+            mycoord(i,1), mycoord(i,2), mycoord(i,3) );
+    end
+
+    fclose(fid);
+
+end
+
+if 1 % generate XML file
+    outfile='labels_dartel_BNAtlas.xml';
+    fid=fopen(outfile, 'w');
+    VOL=1.5^3/1000;
+    for i=1:nROI
+        Vox = length(find(BN==i));
+        Vol = Vox*VOL;
+        fprintf(fid, '<label><index>%d', i);
+        fprintf(fid, '</index><short_name>%s', myname.Var3{i});
+        fprintf(fid, '</short_name><name>%s</name><RGBA></RGBA><XYZmm>', myname.Var3{i});
+        fprintf(fid, '%d,%d,%d</XYZmm></label>\n', mycoord(i,1), mycoord(i,2), mycoord(i,3));
+    end
+
+    fclose(fid);
+end
+
+end
